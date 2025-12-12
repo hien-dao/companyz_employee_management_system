dd4fe0fc8a881763f529379699aef1906157bb31
Hien Dao <hienqdao5@gmail.com>
2025-12-11 22:04:49 -0500

    Make login page look better

--- src/main/java/com/companyz/ems/ui/LoginUI.java
+++ src/main/java/com/companyz/ems/ui/LoginUI.java
@@ -6,6 +6,7 @@ import java.util.function.Consumer;
 import com.companyz.ems.security.SessionContext;
 import com.companyz.ems.services.UserService;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,8 +24,18 @@ public class LoginUI {
 
     public Scene build(Stage stage, Consumer<SessionContext> onLoginSuccess) {
         VBox root = new VBox(20);
+        root.setAlignment(Pos.CENTER);
+
+        Label title = new Label("Employee Management System Login");
+
         TextField usernameField = new TextField();
+        usernameField.setPromptText("Username");
+        usernameField.setMaxWidth(150);   // keep compact
+
         PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.setMaxWidth(150);   // keep compact
+
         Label errorLabel = new Label();
         Button loginButton = new Button("Login");
 
@@ -39,10 +50,12 @@ public class LoginUI {
             }
         });
 
-        root.getChildren().addAll(new Label("Username:"), usernameField,
-                                  new Label("Password:"), passwordField,
-                                  loginButton, errorLabel);
+        VBox formBox = new VBox(10);
+        formBox.setAlignment(Pos.CENTER);
+        formBox.getChildren().addAll(usernameField, passwordField, loginButton, errorLabel);
+
+        root.getChildren().addAll(title, formBox);
+
         return new Scene(root, 400, 300);
     }
 }
-
